9c83b7d573f95eafd20b39ce05e601a579f79058
Magnus Johannes Schröder <magnus.schroeder@esnhamburg.de>
2026-03-31 22:58:06 +0200

    Refactor

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -eu
 
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 chown -R 15371:15371 "$ROOT_DIR/pretix/data/pretix"
 chown 15371:15371 "$ROOT_DIR/pretix/pretix.cfg"
 docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm pretix_init
 docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm --entrypoint sh pretix_restic -c "restic init"
+
+docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm websites_builder
+docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm --entrypoint sh websites_restic -c "restic init"
--- pretix/scripts/restore.sh
+++ pretix/scripts/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 
 docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm --entrypoint sh pretix_restic /scripts/restore.sh
 chown -R 15371:15371 "$ROOT_DIR/pretix/data/pretix"
