cc44518ab400fafb4dd69185a611124e656f015c
Magnus Johannes Schröder <magnus.schroeder@esnhamburg.de>
2026-04-19 03:08:41 +0200

    Refactor

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 chown -R 15371:15371 "$ROOT_DIR/pretix/data/pretix"
 chown 15371:15371 "$ROOT_DIR/pretix/pretix.cfg"
-docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm pretix_init
+docker compose -f "$ROOT_DIR/docker-compose.yml" --profile pretix run --rm pretix_init
 docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm --entrypoint sh pretix_restic -c "restic init"
 
 docker compose -f "$ROOT_DIR/docker-compose.yml" run --rm websites_builder
